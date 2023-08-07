f75de700387c949af0deb429b9b93efec012a8eb
Davichobits <davidruizdeveloper@gmail.com>
2023-08-07 17:49:05 -0500

    lecture 08 completed

--- 08-More-on-Mocking-&-Diving-Deeper/util/http.test.js
+++ 08-More-on-Mocking-&-Diving-Deeper/util/http.test.js
@@ -1,6 +1,7 @@
 import { expect, it, vi } from "vitest";
 
 import { sendDataRequest } from './http';
+import { HttpError } from "./errors";
 
 const testResponseData = {testKey: 'testData'}
 
@@ -39,4 +40,23 @@ it('should convert the provided data to JSON before sending the request', async 
     errorMessage = error;
   }
   expect(testData).not.toBe('Not a string.')
+})
+
+it('should throw an HttpError in case of non-ok response', ()=>{
+  testFetch.mockImplementation((url, options)=>{
+    return new Promise((resolve, reject)=>{
+  
+      const testResponse = {
+        ok: false,
+        json() {
+          return new Promise((resolve, reject)=>{
+            resolve(testResponseData)
+          })
+        }
+      }
+      resolve(testResponse)
+    })
+  })
+  const testData = {key: 'test'}
+  return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError)
 })
